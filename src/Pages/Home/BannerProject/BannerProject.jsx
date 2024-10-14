f6e16a2640edf1cf9fa87100c540b33015dbef66
webcodesky1 <webcodesky1@gmail.com>
2024-10-15 04:43:23 +0600

    for mobile device show 4 card after click all but show all

--- src/Pages/Home/BannerProject/BannerProject.jsx
+++ src/Pages/Home/BannerProject/BannerProject.jsx
@@ -1,5 +1,89 @@
-// import img1 from '../../../assets/bannerImage/b1.png'
-// import img2 from '../../../assets/bannerImage/b2.png'
+// // import img1 from '../../../assets/bannerImage/b1.png'
+// // import img2 from '../../../assets/bannerImage/b2.png'
+// import img3 from "../../../assets/bannerImage/b3.png";
+// import img4 from "../../../assets/bannerImage/b4.png";
+// import img5 from "../../../assets/bannerImage/b5.png";
+// import img6 from "../../../assets/bannerImage/b6.png";
+// import img7 from "../../../assets/bannerImage/b7.png";
+// import img8 from "../../../assets/bannerImage/b8.png";
+// import img9 from "../../../assets/bannerImage/b9.png";
+// import img10 from "../../../assets/bannerImage/b10.png";
+// import img11 from "../../../assets/bannerImage/b11.png";
+// import img12 from "../../../assets/bannerImage/b12.png";
+// // import img13 from "../../../assets/bannerImage/b13.png";
+// // import img14 from "../../../assets/bannerImage/b14.png";
+
+// const BannerProject = () => {
+//   return (
+//     <div className="relative max-sm:pt-[1350px]">
+//       <div className="container mx-auto ">
+//         <div className="relative flex flex-wrap justify-center mx-auto gap-4 ">
+//           <div className="flex flex-col gap-4">
+//             <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+//               <img src={img3} alt="" />
+//             </div>
+//             <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+//               <img src={img4} alt="" />
+//             </div>
+//           </div>
+//           <div className="flex flex-col gap-4 md:mt-[80px]">
+//             <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+//               <img src={img5} alt="" />
+//             </div>
+//             <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+//               <img src={img6} alt="" />
+//             </div>
+//           </div>
+//           <div className="flex flex-col gap-4 md:mt-[230px]">
+//             <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+//               <img src={img7} alt="" />
+//             </div>
+//             <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+//               <img src={img8} alt="" />
+//             </div>
+//           </div>
+//           <div className="flex flex-col gap-4 md:mt-[80px]">
+//             <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+//               <img src={img9} alt="" />
+//             </div>
+//             <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+//               <img src={img10} alt="" />
+//             </div>
+//           </div>
+//           <div className="flex flex-col gap-4">
+//             <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+//               <img src={img11} alt="" />
+//             </div>
+//             <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+//               <img src={img12} alt="" />
+//             </div>
+//           </div>
+//           <div className="absolute -top-[1300px] md:-top-[160px] md:-left-[110px] flex flex-col gap-4 md:gap-0">
+//             <div className="md:h-[297px] md:w-[236px] w-full rounded-xl">
+//               <img src={img4} alt="" className="w-full h-full object-cover" />
+//             </div>
+//             <div className="md:h-[297px] md:w-[236px] w-full rounded-xl">
+//               <img src={img11} alt="" className="w-full h-full object-cover" />
+//             </div>
+//           </div>
+
+//           <div className="absolute -top-[650px] md:-top-[160px] md:-right-[110px] flex flex-col gap-4 md:gap-0">
+//             <div className="md:h-[297px] md:w-[236px] w-full rounded-xl">
+//               <img src={img3} alt="" className="w-full h-full object-cover" />
+//             </div>
+//             <div className="md:h-[297px] md:w-[236px] w-full rounded-xl">
+//               <img src={img11} alt="" className="w-full h-full object-cover" />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default BannerProject;
+
+import { useState } from "react";
 import img3 from "../../../assets/bannerImage/b3.png";
 import img4 from "../../../assets/bannerImage/b4.png";
 import img5 from "../../../assets/bannerImage/b5.png";
@@ -10,55 +94,79 @@ import img9 from "../../../assets/bannerImage/b9.png";
 import img10 from "../../../assets/bannerImage/b10.png";
 import img11 from "../../../assets/bannerImage/b11.png";
 import img12 from "../../../assets/bannerImage/b12.png";
-// import img13 from "../../../assets/bannerImage/b13.png";
-// import img14 from "../../../assets/bannerImage/b14.png";
 
 const BannerProject = () => {
+  const [showAll, setShowAll] = useState(false);
+
+  const handleShowAll = () => {
+    setShowAll(!showAll);
+  };
+
   return (
     <div className="relative max-sm:pt-[1350px]">
       <div className="container mx-auto ">
         <div className="relative flex flex-wrap justify-center mx-auto gap-4 ">
-          <div className="flex flex-col gap-4">
-            <div className="md:h-[297px] md:w-[236px] rounded-xl ">
-              <img src={img3} alt="" />
-            </div>
-            <div className="md:h-[297px] md:w-[236px] rounded-xl ">
-              <img src={img4} alt="" />
-            </div>
+          {/* Initially visible cards */}
+          {showAll && (
+            <>
+              <div className="flex flex-col gap-4">
+                <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+                  <img src={img3} alt="" />
+                </div>
+                <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+                  <img src={img4} alt="" />
+                </div>
+              </div>
+              <div className="flex flex-col gap-4 md:mt-[80px]">
+                <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+                  <img src={img5} alt="" />
+                </div>
+                <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+                  <img src={img6} alt="" />
+                </div>
+              </div>
+
+              {/* Conditionally rendered cards */}
+
+              <div className="flex flex-col gap-4 md:mt-[230px]">
+                <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+                  <img src={img7} alt="" />
+                </div>
+                <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+                  <img src={img8} alt="" />
+                </div>
+              </div>
+              <div className="flex flex-col gap-4 md:mt-[80px]">
+                <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+                  <img src={img9} alt="" />
+                </div>
+                <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+                  <img src={img10} alt="" />
+                </div>
+              </div>
+              <div className="flex flex-col gap-4">
+                <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+                  <img src={img11} alt="" />
+                </div>
+                <div className="md:h-[297px] md:w-[236px] rounded-xl ">
+                  <img src={img12} alt="" />
+                </div>
+              </div>
+            </>
+          )}
+
+          {/* Show All / Show Less button */}
+          <div className="w-full flex justify-center mt-6 md:hidden">
+            <button
+              className="px-6 py-3 bg-[#FF6600] text-white rounded-full"
+              onClick={handleShowAll}
+            >
+              {showAll ? "Show Less" : "Show All"}
+            </button>
           </div>
-          <div className="flex flex-col gap-4 md:mt-[80px]">
-            <div className="md:h-[297px] md:w-[236px] rounded-xl ">
-              <img src={img5} alt="" />
-            </div>
-            <div className="md:h-[297px] md:w-[236px] rounded-xl ">
-              <img src={img6} alt="" />
-            </div>
-          </div>
-          <div className="flex flex-col gap-4 md:mt-[230px]">
-            <div className="md:h-[297px] md:w-[236px] rounded-xl ">
-              <img src={img7} alt="" />
-            </div>
-            <div className="md:h-[297px] md:w-[236px] rounded-xl ">
-              <img src={img8} alt="" />
-            </div>
-          </div>
-          <div className="flex flex-col gap-4 md:mt-[80px]">
-            <div className="md:h-[297px] md:w-[236px] rounded-xl ">
-              <img src={img9} alt="" />
-            </div>
-            <div className="md:h-[297px] md:w-[236px] rounded-xl ">
-              <img src={img10} alt="" />
-            </div>
-          </div>
-          <div className="flex flex-col gap-4">
-            <div className="md:h-[297px] md:w-[236px] rounded-xl ">
-              <img src={img11} alt="" />
-            </div>
-            <div className="md:h-[297px] md:w-[236px] rounded-xl ">
-              <img src={img12} alt="" />
-            </div>
-          </div>
-          <div className="absolute -top-[1300px] md:-top-[160px] md:-left-[110px] flex flex-col gap-4 md:gap-0">
+
+          {/* Absolute positioned images */}
+          <div className="absolute -top-[1300px] md:-top-[160px] md:-left-[110px] flex flex-col gap-4 ">
             <div className="md:h-[297px] md:w-[236px] w-full rounded-xl">
               <img src={img4} alt="" className="w-full h-full object-cover" />
             </div>
@@ -67,12 +175,12 @@ const BannerProject = () => {
             </div>
           </div>
 
-          <div className="absolute -top-[650px] md:-top-[160px] md:-right-[110px] flex flex-col gap-4 md:gap-0">
+          <div className="absolute -top-[650px] md:-top-[160px] md:-right-[110px] flex flex-col gap-4 ">
             <div className="md:h-[297px] md:w-[236px] w-full rounded-xl">
-              <img src={img3} alt="" className="w-full h-full object-cover" />
+              <img src={img4} alt="" className="w-full h-full object-cover" />
             </div>
             <div className="md:h-[297px] md:w-[236px] w-full rounded-xl">
-              <img src={img11} alt="" className="w-full h-full object-cover" />
+              <img src={img4} alt="" className="w-full h-full object-cover" />
             </div>
           </div>
         </div>
