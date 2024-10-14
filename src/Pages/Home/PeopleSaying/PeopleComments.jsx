f43edf2add0fdc3660be1058198238044df37c9b
Mahmud Hasan <mahmudhassan11752@gmail.com>
2024-10-14 14:39:29 +0600

    compleate about us section

---
+++ src/Pages/Home/PeopleSaying/PeopleComments.jsx
@@ -0,0 +1,243 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import img1 from "../../../assets/peopleComments/Container1.png";
+import img2 from "../../../assets/peopleComments/Container2.png";
+import img3 from "../../../assets/peopleComments/Container3.png";
+import { FaRegStar, FaStar } from "react-icons/fa";
+
+const PeopleComments = () => {
+  return (
+    <div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        breakpoints={{
+          320: {
+            slidesPerView: 1.5,
+            spaceBetween: 10,
+          },
+          640: {
+            slidesPerView: 1.5,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 2.5,
+            spaceBetween: 30,
+          },
+         
+        }}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="relative">
+            <img
+              className="w-[90px] h-[90px] absolute -top-[50px] left-[24px]"
+              src={img1}
+              alt=""
+            />
+            <div className="md:w-[269px] md:h-[402px] flex-shrink-0 rounded-[8px] bg-[#FFF] p-6 mt-20 space-y-2">
+              <div className="flex pt-[45px] pb-6">
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaRegStar />
+                </span>
+              </div>
+              <h2 className="text-[#2B2B2B] text-[24px] font-medium leading-[31.992px]">
+                Selena Gomz
+              </h2>
+              <p className="text-[#575757] text-[14px] font-medium leading-[150%]">
+                22 Years
+              </p>
+              <p className="text-[#575757] text-[16px] font-normal leading-[150%] tracking-[-0.32px]">
+                The salad is fresh!!! Don't ask about the sauce again, it's
+                really delicious, it's going to be routine. I recommend this
+                salad to all of you guys! because they really take care of the
+                quality.
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img
+              className="w-[90px] h-[90px] absolute -top-[50px] left-[24px]"
+              src={img2}
+              alt=""
+            />
+            <div className="md:w-[269px] md:h-[402px] flex-shrink-0 rounded-[8px] bg-[#FFF] p-6 mt-20 space-y-2">
+              <div className="flex pt-[45px] pb-6">
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaRegStar />
+                </span>
+              </div>
+              <h2 className="text-[#2B2B2B] text-[24px] font-medium leading-[31.992px]">
+                Selena Gomz
+              </h2>
+              <p className="text-[#575757] text-[14px] font-medium leading-[150%]">
+                22 Years
+              </p>
+              <p className="text-[#575757] text-[16px] font-normal leading-[150%] tracking-[-0.32px]">
+                The salad is fresh!!! Don't ask about the sauce again, it's
+                really delicious, it's going to be routine. I recommend this
+                salad to all of you guys! because they really take care of the
+                quality.
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img
+              className="w-[90px] h-[90px] absolute -top-[50px] left-[24px]"
+              src={img3}
+              alt=""
+            />
+            <div className="md:w-[269px] md:h-[402px] flex-shrink-0 rounded-[8px] bg-[#FFF] p-6 mt-20 space-y-2">
+              <div className="flex pt-[45px] pb-6">
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaRegStar />
+                </span>
+              </div>
+              <h2 className="text-[#2B2B2B] text-[24px] font-medium leading-[31.992px]">
+                Selena Gomz
+              </h2>
+              <p className="text-[#575757] text-[14px] font-medium leading-[150%]">
+                22 Years
+              </p>
+              <p className="text-[#575757] text-[16px] font-normal leading-[150%] tracking-[-0.32px]">
+                The salad is fresh!!! Don't ask about the sauce again, it's
+                really delicious, it's going to be routine. I recommend this
+                salad to all of you guys! because they really take care of the
+                quality.
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img
+              className="w-[90px] h-[90px] absolute -top-[50px] left-[24px]"
+              src={img1}
+              alt=""
+            />
+            <div className="md:w-[269px] md:h-[402px] flex-shrink-0 rounded-[8px] bg-[#FFF] p-6 mt-20 space-y-2">
+              <div className="flex pt-[45px] pb-6">
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaRegStar />
+                </span>
+              </div>
+              <h2 className="text-[#2B2B2B] text-[24px] font-medium leading-[31.992px]">
+                Selena Gomz
+              </h2>
+              <p className="text-[#575757] text-[14px] font-medium leading-[150%]">
+                22 Years
+              </p>
+              <p className="text-[#575757] text-[16px] font-normal leading-[150%] tracking-[-0.32px]">
+                The salad is fresh!!! Don't ask about the sauce again, it's
+                really delicious, it's going to be routine. I recommend this
+                salad to all of you guys! because they really take care of the
+                quality.
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img
+              className="w-[90px] h-[90px] absolute -top-[50px] left-[24px]"
+              src={img2}
+              alt=""
+            />
+            <div className="md:w-[269px] md:h-[402px] flex-shrink-0 rounded-[8px] bg-[#FFF] p-6 mt-20 space-y-2">
+              <div className="flex pt-[45px] pb-6">
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaStar />
+                </span>
+                <span className="text-[#8FD14F]">
+                  <FaRegStar />
+                </span>
+              </div>
+              <h2 className="text-[#2B2B2B] text-[24px] font-medium leading-[31.992px]">
+                Selena Gomz
+              </h2>
+              <p className="text-[#575757] text-[14px] font-medium leading-[150%]">
+                22 Years
+              </p>
+              <p className="text-[#575757] text-[16px] font-normal leading-[150%] tracking-[-0.32px]">
+                The salad is fresh!!! Don't ask about the sauce again, it's
+                really delicious, it's going to be routine. I recommend this
+                salad to all of you guys! because they really take care of the
+                quality.
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default PeopleComments;
--- src/Pages/Home/PeopleSaying/PeopleSaying.jsx
+++ src/Pages/Home/PeopleSaying/PeopleSaying.jsx
@@ -1,26 +1,37 @@
+import { FaArrowRight } from "react-icons/fa";
 import Container from "../../../Components/Ui/Container/Container";
+import { FaArrowLeft } from "react-icons/fa6";
+import PeopleComments from "./PeopleComments";
 
 const PeopleSaying = () => {
   return (
     <div className="bg-[#FFF6EA]">
       <Container>
-        <div className="flex gap-[80px] pt-[120px] pb-[82px]">
+        <div className="flex flex-col lg:flex-row gap-8 lg:gap-[80px] pt-[60px] lg:pt-[120px] pb-[40px] lg:pb-[82px]">
           <div className="flex-[1]">
-            <h1>
-              What Are People Saying{" "}
-              <span className="text-[#FF6600]">About Us</span>{" "}
+            <h1 className="text-2xl lg:text-4xl font-bold">
+              What Are People Saying
+              <span className="text-[#FF6600]"> About Us</span>
             </h1>
-            <p className="mont pt-[30px] pb-[32px] text-[#CCCCCC]">
-              We are very happy if you are satisfied with our service and
-              products, let's read pure reviews from customers who bought our
-              products.
+            <p className="mont pt-6 lg:pt-[30px] pb-6 lg:pb-[32px] text-[#CCCCCC]">
+              We are very happy if you are satisfied with our service and products, let's read pure reviews from customers who bought our products.
             </p>
             <p>
-              <span className="text-[48px] font-bold leading-[64px]">02</span>
-              <span className="text-[#CCCCCC]">/ 05</span>
+              <span className="text-4xl lg:text-[48px] font-bold leading-tight">02</span>
+              <span className="text-[#CCCCCC]"> / 05</span>
             </p>
+            <div className="flex gap-4 pt-6">
+              <button className="w-12 h-12 lg:w-[56px] lg:h-[56px] text-white flex items-center justify-center rounded-lg bg-[rgba(255,102,0,0.5)]">
+                <FaArrowLeft />
+              </button>
+              <button className="w-12 h-12 lg:w-[56px] lg:h-[56px] text-white flex items-center justify-center rounded-lg bg-[#F60]">
+                <FaArrowRight />
+              </button>
+            </div>
           </div>
-          <div className="flex-[2] bg-slate-600">hhh</div>
+          <div className="flex-[2] overflow-hidden">
+            <PeopleComments />
+          </div>
         </div>
       </Container>
     </div>
