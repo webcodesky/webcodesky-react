eea93805df8f42563cdaeb2f92559e2f012ccdb8
webcodesky1 <webcodesky1@gmail.com>
2024-10-26 03:06:11 +0600

    add our work page with api

---
+++ src/Pages/OurWorks/WorksCard/WorksCard.jsx
@@ -0,0 +1,24 @@
+const WorksCard = ({ category }) => {
+  const { name } = category || {};
+  return (
+    <div>
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorksCard;
